720e57c61a8d9dbce486744af8b4c9da4cd85465
nitinkumarpals <nitinpal900@gmail.com>
2026-03-15 00:24:33 +0530

    refactor: remove unused `Query` and `child_process` imports.

--- node/src/app.controller.ts
+++ node/src/app.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { exec } from 'child_process';
 
 @Controller()
 export class AppController {
